21ff62bea8c668b6c8744a747dfa9f360499dfef
Kelton Kowalchuk <hello@kelton.ca>
2022-04-12 11:43:17 -0700

    Initial commit

---
+++ zsh/pure.zsh
@@ -0,0 +1,4 @@
+# https://github.com/sindresorhus/pure
+fpath+=$HOME/.zsh/pure
+autoload -U promptinit; promptinit
+prompt pure
---
+++ zsh/thinkific.zsh
@@ -0,0 +1,3 @@
+# Thinkific-specific Setup
+ssh-add --apple-load-keychain &> /dev/null
+export THINKIFICPATH=/Users/kelton/Thinkific
