748e87c6dd36853f80639000beea2cc331507794
Kelton Kowalchuk <hello@kelton.ca>
2022-04-12 12:20:36 -0700

    fix: add bash to init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 if [[ ! -d $HOME/.oh-my-zsh ]]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
