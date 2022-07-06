3939895b77dd2177bfd87e27db117cd72f71b10c
Kelton Kowalchuk <hello@kelton.ca>
2022-07-06 15:47:20 -0700

    refactor: move mac-setup & Brewfile to root directory and remove init.sh script

---
+++ mac-setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Install Homebrew if not installed
+if [[ ! $(which brew) ]]; then
+  echo -e "Homebrew not installed, installing now..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle --file $DOTFILES/Brewfile
